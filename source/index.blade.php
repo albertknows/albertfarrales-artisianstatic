47d3ad690c92cf84f7766cc38cc5ffce40678988
albertknows <71308782+albertknows@users.noreply.github.com>
2020-09-15 00:14:24 -0700

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,11 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to My Website</h1>
+    <h1>Welcome to My Own Website</h1>
 
     <p>I was just curious during Coronavirus and decided to see how much it would cost to make a static website.</p>
 
-    <p>So far I've paid 9 dollars for my domain name, Albert Farrales. The hosting is completely free courtesy of Netfliy. (Not Sponsored)</p>
+    <p>So far I've paid 9 dollars for my domain name, albertfarrales.com. The hosting is completely free courtesy of Netfliy. (Not Sponsored)</p>
 
     <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
 
