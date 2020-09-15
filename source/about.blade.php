918c9b3ade3495723c4cffe56cdf26b7f2d9a9bf
albertknows <71308782+albertknows@users.noreply.github.com>
2020-09-15 00:17:39 -0700

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -5,12 +5,11 @@
 @section('content')
     <h1>About</h1>
 
-    <p>My name is {{ $page->owner->name }}</p>
+    <p>My name is Albert Farrales and I'm currently 19 years old. I'm a senior about to graduate in two quarters. After college I plan to move to Seattle and hopefully start a career in IT. So far I have an internship as an Account Manager at OMT Enterprises Inc. and am attending classes online.</p>
 
     <h2>Links:</h2>
 
     <ul>
-        <li><a href="https://twitter.com/{{ $page->owner->twitter }}" target="_blank">Twitter</a></li>
-        <li><a href="https://github.com/{{ $page->owner->github }}" target="_blank">GitHub</a></li>
+        <li><a href="https://www.linkedin.com/in/albert-farrales-181547107/" target="_blank">LinkedIn</a></li>
     </ul>
 @endsection
