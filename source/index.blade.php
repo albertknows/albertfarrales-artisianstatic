e45f202457be12fff0161003466724faa3d30cdf
albertknows <71308782+albertknows@users.noreply.github.com>
2020-09-15 00:22:31 -0700

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,11 +3,11 @@
 @section('content')
     <h1>Welcome to My Own Website</h1>
 
-    <p>I was just curious during Coronavirus and decided to see how much it would cost to make a static website.</p>
-
-    <p>So far I've paid 9 dollars for my domain name, albertfarrales.com. The hosting is completely free courtesy of Netfliy. (Not Sponsored)</p>
+    <p>I was just curious during Coronavirus and decided to see how much it would cost to make a static website. So far I've paid 9 dollars for my domain name, albertfarrales.com. The hosting is completely free courtesy of Netfliy. (Not Sponsored)</p>
 
     <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
+    
+    <p>Feel free to follow me on LinkedIn using the About Tab or contact me using the Contact Tab above.</p>
 
     <h2 id="test">Asset Compilation Test</h2>
 
