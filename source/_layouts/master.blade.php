d035c322e42a91820601e48bf226375dc11d7787
albertknows <71308782+albertknows@users.noreply.github.com>
2020-09-15 00:39:15 -0700

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -40,8 +40,8 @@
             <small>
                 &copy; <span data-year></span> {{ $page->owner->name }} &nbsp;&bull;&nbsp;
                 <a href="/feed.atom">RSS</a> &nbsp;&bull;&nbsp;
-                Maintained by Ranie Santos &nbsp;&bull;&nbsp;
-                <a href="https://github.com/raniesantos/artisan-static">GitHub repo</a>.
+                Maintained by Albert Farrales &nbsp;&bull;&nbsp;
+                <a href="https://github.com/albertknows/albertfarrales-artisianstatic">GitHub repo</a>.
             </small>
         </footer>
     </section>
